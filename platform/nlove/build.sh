f48b51cb74dc28329fa92359654335119c35b660
Bart van Strien <bart@bartbes.ath.cx>
2010-10-24 22:45:57 +0200

    Improved build system, now uses multiple CMakeLists and XCompile files.

--- platform/nlove/build.sh
+++ platform/nlove/build.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 
-cp platform/nlove/CMakeLists.txt src/CMakeLists.txt
-mkdir platform/nlove/build
+if [ -z $PLATFORM ]; then
+	PLATFORM=$1
+fi
+
+if [ -z $PLATFORM ]; then
+	PLATFORM="Generic"
+fi
+
+if [ -z $TOOLCHAIN ]; then
+	TOOLCHAIN=$2
+fi
+
+if [ -z $TOOLCHAIN ]; then
+	TOOLCHAIN=$PLATFORM
+fi
+
+cp platform/nlove/CMakeLists-${PLATFORM}.txt src/CMakeLists.txt
+mkdir platform/nlove/build 2> /dev/null
 cd platform/nlove/build
-cmake ../../../src/
+if [ "$TOOLCHAIN" = "Generic" ]; then
+	cmake ../../../src/
+else
+	cmake ../../../src/ -DCMAKE_TOOLCHAIN_FILE=../XCompile-${TOOLCHAIN}.txt
+fi
 make
 cp nlove ../../../
