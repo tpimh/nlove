9e13ccbd4edfc43537a01e6685329422f2e125be
Bart van Strien <bart@bartbes.ath.cx>
2010-10-25 23:20:58 +0200

    Another offset fix

--- src/modules/graphics/sdl/Glyph.cpp
+++ src/modules/graphics/sdl/Glyph.cpp
@@ -49,11 +49,11 @@ namespace sdl
 		if (screen == 0 || surface == 0)
 			return;
 		SDL_Rect destrect;
-		destrect.x = x-ox;
-		destrect.y = y-oy;
+		destrect.x = x-fabs(ox*cos(angle))-fabs(oy*sin(angle));
+		destrect.y = y-fabs(oy*cos(angle))-fabs(ox*sin(angle));
 		if (angle != 0 || sx != 1 || sy != 1)
 		{
-			SDL_Surface *rotozoomed = rotozoomSurfaceXY(surface, LOVE_TODEG(angle), sx, sy, SMOOTHING_OFF);
+			SDL_Surface *rotozoomed = rotozoomSurfaceXY(surface, -LOVE_TODEG(angle), sx, sy, SMOOTHING_OFF);
 			SDL_BlitSurface(rotozoomed, 0, screen, &destrect);
 			SDL_FreeSurface(rotozoomed);
 		}
